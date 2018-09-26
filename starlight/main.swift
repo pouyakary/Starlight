aefcdf652b895a4a1d622fe07e7c410131d0f6a5
pmkary <pouya@kary.us>
2018-09-26 18:38:52 +0330

    Initial Commit

---
+++ starlight/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  starlight
+//
+//  Created by Pouya Kary on 7/4/1397 .
+//  Copyright © 1397 Pouya Kary. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
